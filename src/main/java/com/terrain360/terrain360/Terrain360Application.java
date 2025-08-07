7cc8074bd120277164b9448d1513f0b086e0ae40
HamzaLachiq <lachiq.hamza37@gmail.com>
2025-08-07 14:38:33 +0100

    first commit

---
+++ src/main/java/com/terrain360/terrain360/Terrain360Application.java
@@ -0,0 +1,13 @@
+package com.terrain360.terrain360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Terrain360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Terrain360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/terrain360/terrain360/Terrain360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terrain360.terrain360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Terrain360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
